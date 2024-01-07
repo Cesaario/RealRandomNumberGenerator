c5c2ac1d951f62545d31c11e78e6d0b4601c1e78
Gustavo <gs.cesario1998@gmail.com>
2024-01-07 17:55:40 -0300

    Script para rodar o dado

---
+++ Arduino/RRNG/RRNG.ino
@@ -0,0 +1,38 @@
+// Programa para controle de motor por comando serial
+// Autor: Gustavo Cesário
+
+int PINO_MOTOR = 6;
+int PINO_MOTOR_INVERSO = 7;
+
+boolean teste = false;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(PINO_MOTOR, OUTPUT);
+  pinMode(PINO_MOTOR_INVERSO, OUTPUT);
+  digitalWrite(PINO_MOTOR, LOW);
+  digitalWrite(PINO_MOTOR_INVERSO, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void loop()
+{
+  delay(1000);
+  if (Serial.available() <= 0 && !teste)
+    return;
+
+  String comando = Serial.readStringUntil(';');
+  comando.trim();
+
+  if (comando == "RODAR_DADO" || teste) {
+    digitalWrite(PINO_MOTOR, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1500);
+    digitalWrite(PINO_MOTOR, LOW);
+    digitalWrite(PINO_MOTOR_INVERSO, HIGH);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(750);
+    digitalWrite(PINO_MOTOR_INVERSO, LOW);
+  }
+}
